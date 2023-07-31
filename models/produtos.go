44dbc7c2b18a9bf340776169cbbed4ce5b068d58
Ygor Silva <ygor98ss@tutanota.com>
2023-07-31 10:46:31 -0300

    project 01

--- models/produtos.go
+++ models/produtos.go
@@ -1,6 +1,8 @@
 package models
 
-import "github.com/yssgor/alura_go/db"
+import (
+	"github.com/yssgor/alura_go/db"
+)
 
 type Produto struct {
 	Id         int
@@ -31,6 +33,7 @@ func BuscaProdutos() []Produto {
 			panic(err.Error())
 		}
 
+		p.Id = id
 		p.Nome = nome
 		p.Descricao = descricao
 		p.Preco = preco
@@ -41,4 +44,68 @@ func BuscaProdutos() []Produto {
 
 	defer db.Close()
 	return produtos
+}
+
+func CriarNovoProduto(nome, descricao string, preco float64, quantidade int) {
+	db := db.ConectaDB()
+	insereDadosNoBanco, err := db.Prepare("insert into produtos(nome, descricao, preco, quantidade) values($1, $2, $3, $4)")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	insereDadosNoBanco.Exec(nome, descricao, preco, quantidade)
+	defer db.Close()
+}
+
+func DeletaProduto(id string) {
+	db := db.ConectaDB()
+	deletarOProduto, err := db.Prepare("delete from produtos where id=$1")
+	if err != nil {
+		panic(err.Error())
+	}
+	deletarOProduto.Exec(id)
+	defer db.Close()
+}
+
+func EditaProduto(id string) Produto {
+	db := db.ConectaDB()
+
+	produtoDoBranco, err := db.Query("select * from produtos where id=$1", id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	produtoParaAtualizar := Produto{}
+
+	for produtoDoBranco.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = produtoDoBranco.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		produtoParaAtualizar.Id = id
+		produtoParaAtualizar.Nome = nome
+		produtoParaAtualizar.Descricao = descricao
+		produtoParaAtualizar.Preco = preco
+		produtoParaAtualizar.Quantidade = quantidade
+
+	}
+	defer db.Close()
+	return produtoParaAtualizar
+}
+
+func AtualizaProduto(id, quantidade int, nome, descricao string, preco float64) {
+	db := db.ConectaDB()
+
+	atualizaProduto, err := db.Prepare("update produtos set nome=$1, descricao=$2, preco=$3, quantidade=$4 where id=$5")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	atualizaProduto.Exec(nome, descricao, preco, quantidade, id)
+	defer db.Close()
 }
